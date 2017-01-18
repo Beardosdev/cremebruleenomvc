e873c090876f79f4721729e329cf8c2834b23161
saliz <saliz@DESKTOP-36V1N25>
2017-01-18 13:30:32 -0500

    Calendar initial commit: Includes new callendar pages and base html, javascript connection and empty php page.

---
+++ JMCommunityProject/clientCode/CalendarCalc.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+
+    $(".days").append("<li>Hello world!</li>");
+
+    //function currentMonth() {
+
+    //    //array to 
+    //    var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July',
+    //	'August', 'September', 'October', 'November', 'December');
+    //    var month = getMonth();
+
+    //    $("#output").html(response);
+
+});
---
+++ JMCommunityProject/pages/Caleder.php
@@ -0,0 +1,21 @@
+<?php
+	include('header.html');
+
+	echo "
+
+	<div class='hero'>
+
+		<h1> JM </h1>
+
+	</div>
+
+	<div class='white'>
+
+		<h2> What do we do? </h2>
+
+	</div>
+	";
+
+
+	include('footer.html');
+?>
