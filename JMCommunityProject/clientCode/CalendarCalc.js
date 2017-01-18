a31b224be5dfb6dadb2c4be9456ca6b542563822
saliz <saliz@DESKTOP-36V1N25>
2017-01-18 15:53:37 -0500

    Calendar populatig dates

--- JMCommunityProject/clientCode/CalendarCalc.js
+++ JMCommunityProject/clientCode/CalendarCalc.js
@@ -10,10 +10,51 @@
     $(".output").append("<h1>" + m + "</h1><br>");
 
     day = d.getDate();
-    dayWeek = d.getDay();
+
+    var newD = new Date();
+    newD.setDate(d.getDate() - day+1);
+    week = newD.getDay();
+    newD.setDate(newD.getDate() - week);
     
-    popMonth = d - day;
-    alert(dayWeek.toString());
-    $(".days").append("<li>"+ day +"</li>");
+    alert(week);
+    var text = "";
+    var i = 0;
+    while (i < 35) {
+        if (newD == d) {
+            text += '<li><span class="active">' + newD.toString() + '</span></li>';
+        }
+        else {
+            text += "<li>";
+            text += newD.toString();
+            text += "</li>";
+        }
+        newD.setDate(newD.getDate() + 1);
+        i++;
+    }
+
+    //var k = 0;
+    //while (k < dayWeek-2)
+    //{
+    //    text += "<li></li>";
+    //    k++;
+    //}
+
+    //var i = 1;
+    //while (i < day-1) {
+    //    text += "<li>" + i;
+    //    text += "</li>";
+    //    i++;
+    //}
+    //text += '<li><span class="active">' + day + '</span></li>';
+    //var j = day + 1;
+    //while (j < 32)
+    //{
+    //    text += "<li>" + j;
+    //    text += "</li>";
+    //    j++;
+    //}
+   
+
+    $(".days").append(text);
 
 });
